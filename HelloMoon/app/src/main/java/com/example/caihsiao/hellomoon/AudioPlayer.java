a502f944344b07a46476ba691295b3111db3930f
caihsiao <caihsiaoster@gmail.com>
2014-10-25 23:09:20 -0700

    check in AudioPlayer for HelloMoon

---
+++ HelloMoon/app/src/main/java/com/example/caihsiao/hellomoon/AudioPlayer.java
@@ -0,0 +1,35 @@
+package com.example.caihsiao.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by caihsiao on 10/25/14.
+ */
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context context) {
+        stop();
+        mPlayer = MediaPlayer.create(context, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stop();
+            }
+        });
+
+        mPlayer.start();
+    }
+
+
+}
---
+++ HelloMoon/app/src/main/java/com/example/caihsiao/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,75 @@
+package com.example.caihsiao.hellomoon;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HelloMoonFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class HelloMoonFragment extends Fragment {
+    private Button mPlayButton;
+    private Button mStopButton;
+
+    private AudioPlayer mPlayer = new AudioPlayer();
+
+//    // TODO: Rename and change types and number of parameters
+//    public static HelloMoonFragment newInstance(String param1, String param2) {
+//        HelloMoonFragment fragment = new HelloMoonFragment();
+//        Bundle args = new Bundle();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//    public HelloMoonFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+////            mParam1 = getArguments().getString(ARG_PARAM1);
+////            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hello_moon, container, false);
+
+        mPlayButton = (Button) view.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.play(getActivity());
+            }
+        });
+
+        mStopButton = (Button) view.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.stop();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+}
